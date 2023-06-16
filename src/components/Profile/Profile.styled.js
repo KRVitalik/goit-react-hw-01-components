d02af5792de5e49d9e6a2814bcf9708f8df9d273
KRVitalik <karmelitavitalik@gmail.com>
2023-06-16 16:21:48 +0300

    Update Profile.styled.js

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 text-align: center;
 padding: 10px;
-width: 550px;
+width: 350px;
 border: 2px solid gray;
 margin: 10px auto;
 `
